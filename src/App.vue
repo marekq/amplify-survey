1830081cfe8cddd553096eb4a8a5c45ba83788a8
Marek Kuczynski <marek.kuczynski@gmail.com>
2020-12-07 22:33:34 +0100

    added submit to API, added questions, various bugfixes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,57 @@
 <template>
-  <flow-form v-bind:questions="questions" v-bind:language="language" />
+  <div>
+    <flow-form
+      ref="flowform"
+      v-on:complete="onComplete"
+      v-on:submit="onSubmit"
+      v-bind:questions="questions"
+      v-bind:language="language"
+      v-bind:standalone="true"
+    >
+     <template v-slot:complete>
+        <div class="f-section-wrap">
+          <p>
+            <span class="fh2">Thank you!</span>
+            <span class="f-section-text">
+              The survey is completed, please feel free to reach out with any questions to @marekq!
+            </span>
+          </p>
+        </div>  
+      </template>
+
+      <template v-slot:completeButton>
+        <div class="f-submit" v-if="!submitted">
+          <button 
+            class="o-btn-action"
+            ref="button"
+            type="submit"
+            href="#"
+            v-on:click.prevent="onSendData()"
+            aria-label="Press to submit"
+          >
+            <span>{{ language.submitText }}</span>
+          </button>
+          <a class="f-enter-desc"
+            href="#"
+            v-on:click.prevent="onSendData()"
+            v-html="language.formatString(language.pressEnter)">
+          </a>
+        </div>
+      </template>
+    </flow-form>
+  </div>
 </template>
 
 <script>
-  // Import necessary components and classes
-  import FlowForm, { QuestionModel, QuestionType, ChoiceOption, LanguageModel } from '@ditdot-dev/vue-flow-form'
+  import FlowForm, { QuestionModel, QuestionType, ChoiceOption, LanguageModel } from '@ditdot-dev/vue-flow-form';
+  import fetch from "node-fetch";
 
   export default {
-    name: 'example',
+    name: 'vuesurvey',
     components: {
       FlowForm
     },
+
     data() {
       return {
         language: new LanguageModel({}),
@@ -32,10 +73,10 @@
             type: QuestionType.Dropdown,
             options: [
               new ChoiceOption({
-                label: 'Answer one'
+                label: 'Answer three'
               }),
               new ChoiceOption({
-                label: 'Answer two'
+                label: 'Answer four'
               }),
             ]
           }),
@@ -52,30 +93,91 @@
               }),
               new ChoiceOption({
                 label: 'Answer 2'
-              }),
-              new ChoiceOption({
-                label: 'Answer 3'
-              }),
-              new ChoiceOption({
-                label: 'Answer 4'
               })
             ]
           }),
           new QuestionModel({
             title: 'Any other comments?',
-            type: QuestionType.LongText
+            type: QuestionType.LongText,
+            required: false
           })
         ]
+      }
+    },
+    mounted() {
+      document.addEventListener('keyup', this.onKeyListener)
+    },
+    beforeDestroy() {
+      document.removeEventListener('keyup', this.onKeyListener)
+    },
+    methods: {
+      // send data onComplete
+      /* eslint-disable-next-line no-unused-vars */
+      onComplete(completed, questionList) {
+        this.onSendData();
+      },
+
+      // send data onSubmit
+      /* eslint-disable-next-line no-unused-vars */
+      onSubmit(questionList) {
+        this.onSendData();
+      },
+
+      // run onSendData
+      onSendData() {
+
+        // set submitted form status to true
+        this.$refs.flowform.submitted = true;
+        this.submitted = true;
+
+        /* eslint-disable-next-line no-unused-vars */
+        const data = this.getData();
+
+        // TODO - implement API endpoint
+        const url = "https://marek.rocks/"
+
+        console.log('sending post');
+        console.log(data);
+
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+      },
+
+      // get the question data
+      getData() {
+        const data = {
+          questions: [],
+          answers: [],
+        };
+        this.questions.forEach((question) => {
+          if (question.title) {
+            let answer = question.answer;
+            if (typeof answer === "object") {
+              answer = answer.join(", ");
+            }
+            data.questions.push(question.title);
+            data.answers.push(answer);
+          }
+        });
+        return data;
+      },
+
+      // set key listener event for enter to send data
+      onKeyListener($event) {
+        if ($event.key === "Enter" && this.completed && !this.submitted) {
+          this.onSendData();
+        }
       }
     }
   }
 </script>
 
 <style>
-  /* Import Vue Flow Form base CSS */
   @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.css';
-  /* Import one of the Vue Flow Form CSS themes (optional) */
   @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-minimal.css';
-  /* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-green.css'; */
-  /* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-purple.css'; */
 </style>
