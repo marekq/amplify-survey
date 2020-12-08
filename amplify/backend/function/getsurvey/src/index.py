0059a9f366dea8571e0efbf277413a7f794ffd48
Marek Kuczynski <marek.kuczynski@gmail.com>
2020-12-08 21:30:54 +0100

    indent fix on lambda

--- amplify/backend/function/getsurvey/src/index.py
+++ amplify/backend/function/getsurvey/src/index.py
@@ -1,9 +1,10 @@
 import json
 
 def handler(event, context):
+
 	print(event)
-	
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+
+	return {
+		'statusCode': 200,
+		'body': json.dumps('Hello from Lambda!')
+	}
