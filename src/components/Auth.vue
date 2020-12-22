ac9e6cf660093eba95129b5cdf8c4dcbe9b6010a
Marek Kuczynski <marek.kuczynski@gmail.com>
2020-12-22 20:21:40 +0100

    added group add for new users, sign up based on username instead of email

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,15 +1,13 @@
 <template>
   <div class = "auth">
-    <amplify-authenticator username-alias = "email">
+    <amplify-authenticator username-alias = "username">
       <amplify-sign-up
         :form-fields.prop = "signupfields"
         slot = "sign-up"
-        username-alias = "username"
       />
       <amplify-sign-in 
         :form-fields.prop = "loginfields"
         slot = "sign-in" 
-        username-alias = "username"
       />
     </amplify-authenticator>
   </div>
